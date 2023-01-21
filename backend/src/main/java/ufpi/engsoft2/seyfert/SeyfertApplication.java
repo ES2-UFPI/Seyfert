b48bd43e83f3c187b4bcf1f5b308223a514f355f
Járdesson Ribeiro <jardesson.santos@maida.health>
2023-01-21 00:03:14 -0300

    Criação dos projetos

---
+++ backend/src/main/java/ufpi/engsoft2/seyfert/SeyfertApplication.java
@@ -0,0 +1,13 @@
+package ufpi.engsoft2.seyfert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeyfertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeyfertApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ufpi/engsoft2/seyfert/SeyfertApplicationTests.java
@@ -0,0 +1,13 @@
+package ufpi.engsoft2.seyfert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeyfertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
