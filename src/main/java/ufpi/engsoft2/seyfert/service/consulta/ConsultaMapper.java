622ed999f7194dc1a3ce222a62fb991a5473c2c2
Matheus Luís Webber <matheus.webber@hotmail.com>
2023-01-26 09:06:24 -0300

    Criados os arquivos DTO, Mapper e interface Service de Consulta, também foi criado um novo arquivo EntityNoFormMapper para permitir a criação de Mappers para endpoints que precisam apenas do mapeamento entre DTO e a entidade, sem necessidade do form

---
+++ src/main/java/ufpi/engsoft2/seyfert/service/consulta/ConsultaMapper.java
@@ -0,0 +1,24 @@
+package ufpi.engsoft2.seyfert.service.consulta;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.springframework.stereotype.Component;
+
+import ufpi.engsoft2.seyfert.domain.dto.ConsultaDTO;
+import ufpi.engsoft2.seyfert.domain.model.Consulta;
+import ufpi.engsoft2.seyfert.shared.mapper.EntityNoFormMapper;
+
+@Component
+@Mapper(
+    componentModel = "spring",
+    nullValuePropertyMappingStrategy = org.mapstruct.NullValuePropertyMappingStrategy.IGNORE)
+public interface ConsultaMapper extends EntityNoFormMapper<ConsultaDTO, Consulta> {
+    @Mappings({
+        @Mapping(target = "nomeCompletoPaciente", source = "paciente.nomeCompleto"),
+        @Mapping(target = "nomeCompletoMedico", source = "medico.nomeCompleto"),
+        @Mapping(target = "situacaoPagamento", source = "pagamentoConsulta.situacao"),
+        @Mapping(target = "situacaoConsulta", source = "situacao")
+    })
+    ConsultaDTO toDto(Consulta consulta);
+}
