8771de92242967e15a406920d748e5241de26ec1
Járdesson Ribeiro <jardesson.santos@maida.health>
2023-01-31 08:24:10 -0300

    dev_sprint01 Adicionado configuração para permitir requisição externa

--- src/main/java/ufpi/engsoft2/seyfert/controller/ConsultaController.java
+++ src/main/java/ufpi/engsoft2/seyfert/controller/ConsultaController.java
@@ -1,5 +1,7 @@
 package ufpi.engsoft2.seyfert.controller;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ufpi.engsoft2.seyfert.domain.dto.ConsultaDTO;
+import ufpi.engsoft2.seyfert.domain.enums.SituacaoConsulta;
+import ufpi.engsoft2.seyfert.domain.enums.SituacaoPagamento;
 import ufpi.engsoft2.seyfert.service.consulta.ConsultaService;
 
 @RestController
@@ -21,7 +25,20 @@ public class ConsultaController {
 
     @GetMapping("/{uuid}")
     public ResponseEntity<ConsultaDTO> getConsulta(@PathVariable UUID uuid){
-        ConsultaDTO consultaDTO = consultaService.getConsulta(uuid);
-        return ResponseEntity.ok(consultaDTO);
+        // ConsultaDTO consultaDTO = consultaService.getConsulta(uuid);
+
+        ConsultaDTO consultaFake = ConsultaDTO
+            .builder()
+            .nomeCompletoMedico("Medico Teste Fake")
+            .nomeCompletoPaciente("Felipe Santiago Mossoro")
+            .dataAtendimento(LocalDate.now())
+            .descricaoMedica("Uma descrição teste")
+            .horario(LocalTime.now())
+            .situacaoConsulta(SituacaoConsulta.AGUARDANDO_ATENDIMENTO)
+            .situacaoPagamento(SituacaoPagamento.PAGO)
+            .build();
+        return ResponseEntity.ok(consultaFake);
+
+        // return ResponseEntity.ok(consultaDTO);
     }
 }
