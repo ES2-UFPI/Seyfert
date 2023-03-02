ada1aa5788f77671495cfa29d4e219eb546dd3e7
jgbrands <jgfernandesbrandao@gmail.com>
2023-03-02 00:01:58 -0300

    Fixes#43

--- src/main/java/ufpi/engsoft2/seyfert/service/consulta/impl/ConsultaServiceImpl.java
+++ src/main/java/ufpi/engsoft2/seyfert/service/consulta/impl/ConsultaServiceImpl.java
@@ -33,4 +33,22 @@ public class ConsultaServiceImpl implements ConsultaService {
 
         return consultaMapper.toDto(consulta);
     }
+
+    public void cadastrarHorarioDisponivel(UUID medicoUuid, HorarioDisponivelMedico horarioDisponivel){
+        Medico medico = medicoRepository.findById(medicoUuid);
+
+        List<HorarioDisponivelMedico> horarios = medico.getHorariosDisponiveis();
+
+        for (HorarioDisponivelMedico horario : horarios) {
+            if (horario.equals(horarioDisponivel)) {
+                throw new BussinesRuleException("Horario já cadastrado");
+            }
+        }
+
+        horarios.add(horarioDisponivel);
+
+        medico.setHorariosDisponiveis(horarios);
+
+        medicoRepository.save(medico);
+    }
 }
