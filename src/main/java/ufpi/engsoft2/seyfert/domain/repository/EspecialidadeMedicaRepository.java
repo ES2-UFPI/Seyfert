0392f165167c2c9e2f227b085cdc18e08cb9c923
Matheus Luís Webber <matheus.webber@hotmail.com>
2023-03-23 09:57:17 -0300

    Iniciando a criacao dos testes unitarios para o endpoint de solicitacao #59

--- src/main/java/ufpi/engsoft2/seyfert/domain/repository/EspecialidadeMedicaRepository.java
+++ src/main/java/ufpi/engsoft2/seyfert/domain/repository/EspecialidadeMedicaRepository.java
@@ -1,9 +1,12 @@
 package ufpi.engsoft2.seyfert.domain.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ufpi.engsoft2.seyfert.domain.model.EspecialidadeMedica;
 
 public interface EspecialidadeMedicaRepository extends JpaRepository<EspecialidadeMedica, Long> {
     EspecialidadeMedica findByNomeEspecialidade(String nomeEspecialidade);
+    EspecialidadeMedica findByUuid(UUID uuid);
 }
