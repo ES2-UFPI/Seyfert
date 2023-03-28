14d1f6c4074e34a9f67433918c0a1141426de9bc
Matheus Luís Webber <matheus.webber@hotmail.com>
2023-03-27 21:40:37 -0300

    Criado todo o fluxo para o endpoint de login de usuario #61

--- src/test/java/ufpi/engsoft2/seyfert/usuario/UsuarioControllerTest.java
+++ src/test/java/ufpi/engsoft2/seyfert/usuario/UsuarioControllerTest.java
@@ -1,4 +1,4 @@
-package ufpi.engsoft2.seyfert.solicitacao;
+package ufpi.engsoft2.seyfert.usuario;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,26 +24,28 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import ufpi.engsoft2.seyfert.domain.dto.SolicitacaoDTO;
 import ufpi.engsoft2.seyfert.domain.enums.Sexo;
-import ufpi.engsoft2.seyfert.domain.form.SolicitacaoForm;
+import ufpi.engsoft2.seyfert.domain.enums.SituacaoSolicitacao;
 import ufpi.engsoft2.seyfert.domain.model.EspecialidadeMedica;
 import ufpi.engsoft2.seyfert.domain.model.Paciente;
+import ufpi.engsoft2.seyfert.domain.model.Solicitacao;
 import ufpi.engsoft2.seyfert.domain.repository.EspecialidadeMedicaRepository;
 import ufpi.engsoft2.seyfert.domain.repository.PacienteRepository;
 import ufpi.engsoft2.seyfert.domain.repository.SolicitacaoRepository;
-import ufpi.engsoft2.seyfert.service.solicitacao.impl.SolicitacaoServiceImpl;
 
 @SpringBootTest
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 @ActiveProfiles("test")
 @SuppressWarnings("unused")
 @RequiredArgsConstructor
+@AutoConfigureMockMvc
 @TestInstance(Lifecycle.PER_CLASS)
-public class SolicitacaoServiceTest {
+public class UsuarioControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
     @Autowired
     private PacienteRepository pacienteRepository;
 
@@ -53,10 +55,7 @@ public class SolicitacaoServiceTest {
     @Autowired
     private SolicitacaoRepository solicitacaoRepository;
 
-    @Autowired
-    private SolicitacaoServiceImpl solicitacaoService;
-
-    @BeforeEach
+    @BeforeAll
     public void setUp() {
         Paciente paciente = new Paciente();
         paciente.setNome("Teste");
@@ -73,8 +72,28 @@ public class SolicitacaoServiceTest {
         especialidadeMedica.setTempoMedioConsultaEmMinutos(30);
         especialidadeMedica.setUuid(UUID.fromString("8e08cfed-0960-47dd-afe1-d1e5017d874b"));
         especialidadeMedicaRepository.save(especialidadeMedica);
+
+        Solicitacao solicitacao = new Solicitacao();
+        solicitacao.setDataParaAtendimento(LocalDate.now().plusYears(1));
+        solicitacao.setHoraInicial(LocalTime.now().plusHours(1));
+        solicitacao.setHoraFinal(LocalTime.now().plusHours(2));
+        solicitacao.setDescricaoSolicitacao("Teste de solicitacao");
+        solicitacao.setSexoPreferivelDoAtendimento(Sexo.OUTROS);
+        solicitacao.setPaciente(paciente);
+        solicitacao.setEspecialidadeMedica(especialidadeMedica);
+        solicitacaoRepository.save(solicitacao);
+
+        solicitacao = new Solicitacao();
+        solicitacao.setDataParaAtendimento(LocalDate.now().plusYears(1));
+        solicitacao.setHoraInicial(LocalTime.now().plusHours(1));
+        solicitacao.setHoraFinal(LocalTime.now().plusHours(2));
+        solicitacao.setDescricaoSolicitacao("Teste de solicitacao");
+        solicitacao.setSexoPreferivelDoAtendimento(Sexo.OUTROS);
+        solicitacao.setPaciente(paciente);
+        solicitacao.setEspecialidadeMedica(especialidadeMedica);
+        solicitacaoRepository.save(solicitacao);
     }
-    
+  
     @AfterAll
     public void tearDown() {
         solicitacaoRepository.deleteAll();
@@ -83,23 +102,10 @@ public class SolicitacaoServiceTest {
     }
 
     @Test
-    @DisplayName("Deve ser bem sucedido ao cadastrar uma solicitação")
-    public void cadastrarSolicitacao() throws Exception {
-        SolicitacaoForm solicitacaoForm = new SolicitacaoForm();
-        solicitacaoForm.setHoraInicial(LocalTime.now().plusHours(1));
-        solicitacaoForm.setHoraFinal(LocalTime.now().plusHours(2));
-        solicitacaoForm.setDataParaAtendimento(LocalDate.now());
-        solicitacaoForm.setDescricaoSolicitacao("Teste");
-        solicitacaoForm.setSexoPreferivelDoAtendimento(Sexo.MASCULINO);
-        solicitacaoForm.setPacienteUuid(UUID.fromString("f9b360de-d79a-4770-9c15-a62591e67022"));
-        solicitacaoForm.setEspecialidadeMedicaUuid(UUID.fromString("8e08cfed-0960-47dd-afe1-d1e5017d874b"));
-
-        SolicitacaoDTO resposta = solicitacaoService.cadastrar(solicitacaoForm);
-
-        assertEquals(resposta.getHoraInicial(), solicitacaoForm.getHoraInicial());
-        assertEquals(resposta.getHoraFinal(), solicitacaoForm.getHoraFinal());
-        assertEquals(resposta.getDataParaAtendimento(), solicitacaoForm.getDataParaAtendimento());
-        assertEquals(resposta.getDescricaoSolicitacao(), solicitacaoForm.getDescricaoSolicitacao());
-        assertEquals(resposta.getSexoPreferivelDoAtendimento(), solicitacaoForm.getSexoPreferivelDoAtendimento());
+    @DisplayName("Buscar um paciente")
+    public void deveRetornar200AoBuscarUmPaciente() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/usuario")
+        .param("email", "email").param("senha", "senha"))
+        .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 }
